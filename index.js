955255812c52defca784f2b5797ac01defc1073e
Tim Shaker <tmshkr@users.noreply.github.com>
2018-06-30 20:13:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  
+  var newObj = Object.assign({}, object)
+  delete newObj[key]
+  return newObj  
 }
